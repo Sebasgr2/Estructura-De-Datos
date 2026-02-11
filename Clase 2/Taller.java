76c1d47006df579ed0e192d244704aa52e4fcddf
Sebastian Gonzalez Reyes <sebastian.gonzale@campusucc.edu.co>
2026-02-11 21:46:03 +0000

    Ejercicio 2

--- Clase 2/Taller.java
+++ Clase 2/Taller.java
@@ -5,7 +5,8 @@ public class Taller {
     
         Scanner teclado = new Scanner(System.in);
 
-       int[] numeros = new int[8];
+    // Ejercicio 1    
+       int[] numeros = new int[4];
        int suma = 0;
 
        for(int i = 0; i < numeros.length; i++){
@@ -20,6 +21,27 @@ public class Taller {
         teclado.close();
         }
 
+    //Ejervcicio 2
+    int[] a = {10, 20, 15, 8, 12};
+    int[] b = {2,   3,  5, 1,  3};
+    int[] c = {3,   3,  5, 3,  1};
+
+    int[] t = new int[5]; 
+    //reserva en memoria un arreglo de tamaño 5 para almacenar
+    // la operacion del calculo de cada trabajador
+
+    for(int i = 0; i < a.length; i++){
+        t[i] = a[i] + b[i] - c[i];
+        //Almacena en el arreglo t la operacion establecida en el pdf
+
+    for(int i = 0; i < a.length; i++){
+        System.out.print("t[" + i + "]" + t[i]);
+
     }
+
+
+        } 
+
+   }
 
 }
