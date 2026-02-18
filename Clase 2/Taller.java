825285889d2cf8c6cfcef61800fdf11a768ff9d6
Sebastian Gonzalez Reyes <sebastian.gonzale@campusucc.edu.co>
2026-02-18 22:19:33 +0000

    Clase 3

--- Clase 2/Taller.java
+++ Clase 2/Taller.java
@@ -40,5 +40,8 @@ public class Taller {
 
         }
 
+
+        //Ejercicio 3
+        
     }
 }
---
+++ Clase 3/EjecutarMoto.java
@@ -0,0 +1,12 @@
+public class EjecutarMoto {
+    public static void main(String[] args){
+
+        Moto[] m = new Moto[8];
+        
+        m[0] = new Moto(2026, "AKT Turning", "Negra", 125.0, 1250.0);
+        m[1] = new Moto(2025, "Kawasaki", "Verde", 100.0, 25000.0);
+        m[2] = new Moto(2024, "Honda", "Roja", 350.0, 4000.0);
+        m[3] = new Moto(2027, "Honda c90", "DoradoNegro", 90.0, 10000.0);
+    }
+       
+}
